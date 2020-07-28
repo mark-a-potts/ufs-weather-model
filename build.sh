96cf4f8a919ffa550db71318eb10951fba05da3f
Mark Potts <Mark.Potts@noaa.gov>
2020-07-28 14:30:40 -0400

    separate configs for fms and ufs-weather-model-lib

--- build.sh
+++ build.sh
@@ -9,6 +9,13 @@ export CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER:-mpicxx}
 export CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-mpif90}
 
 export NETCDF=${NETCDF:?"Please set NETCDF environment variable"}
+export BACIO_LIB4=${BACIO_LIB4:-/home/mpotts/NCEPLIBS/build-develop/install/bacio-2.4.0/lib/libbacio_4.a}
+export NEMSIO_INC=${NEMSIO_INC:-/home/mpotts/NCEPLIBS/build-develop/install/nemsio-2.5.1/include}
+export NEMSIO_LIB=${NEMSIO_LIB:-/home/mpotts/NCEPLIBS/build-develop/install/nemsio-2.5.1/lib/libnemsio.a}
+export SP_LIBd=${SP_LIBd:-/home/mpotts/NCEPLIBS/build-develop/install/sp-2.3.0/lib/libsp_d.a}
+export W3EMC_LIBd=${W3EMC_LIBd:-/home/mpotts/NCEPLIBS/build-develop/install/w3emc-2.7.0/lib/libw3emc_d.a}
+export W3NCO_LIBd=${W3NCO_LIBd:-/home/mpotts/NCEPLIBS/build-develop/install/w3nco-2.4.0/lib/libw3nco_d.a}
+export NETCDF=${NETCDF}
 export ESMFMKFILE=${ESMFMKFILE:?"Please set ESMFMKFILE environment variable"}
 
 BUILD_DIR=${MYDIR}/build
